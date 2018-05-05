95415713968e278e4eb1b0c8542b2d75b805cfa6
Dmitry <dmkozyrev@rambler.ru>
2018-05-05 20:41:43 +0300

    "Ближайшее число": поиск в глубину, O(n^2)

---
+++ acmp.ru/0621-bfs.cpp
@@ -0,0 +1,68 @@
+#pragma GCC diagnostic ignored "-Wunused-result"
+
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+struct Point { int row, col; };
+
+int main() {
+    // Чтение данных
+    int n; scanf("%d", &n); 
+    std::vector<std::vector<int>> item(n, std::vector<int>(n));
+    for (int row = 0; row < n; ++row) {
+        for (int col = 0; col < n; ++col) {
+            scanf("%d", &item[row][col]);
+        }
+    }
+    // Замена всех ненулевых элементов и добавление их в очередь
+    // из ненулевых элементов и запустим поиск в ширину
+    const int INF = (int)1e9+1;
+    std::vector<int> values{0};
+    std::queue<Point> queue;
+    std::vector<std::vector<int>> dist(n, std::vector<int>(n, INF));
+    for (int row = 0; row < n; ++row) {
+        for (int col = 0; col < n; ++col) {
+            if (item[row][col] != 0) {
+                values.push_back(item[row][col]);
+                item[row][col] = (int)values.size()-1;
+                queue.push({row, col});
+                dist[row][col] = 0;
+            }
+        }
+    }
+    while (!queue.empty()) {
+        int row = queue.front().row;
+        int col = queue.front().col;
+        queue.pop();
+        for (int dr = -1; dr <= 1; ++dr) {
+            for (int dc = -1; dc <= 1; ++dc) {
+                if (dr * dr + dc * dc == 1) {
+                    int nr = dr + row;
+                    int nc = dc + col;
+                    if (!(0 <= nr && nr < n && 0 <= nc && nc < n)) continue;
+                    if (dist[nr][nc] > dist[row][col] + 1) {
+                        item[nr][nc] = item[row][col];
+                        dist[nr][nc] = dist[row][col] + 1;
+                        queue.push({nr, nc});
+                    } else if (dist[nr][nc] == dist[row][col]+1) {
+                        // Достижима из двух точек
+                        if (item[nr][nc] != item[row][col]) {
+                            item[nr][nc] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    char buf[100000];
+    for (int row = 0; row < n; ++row) {
+        int size = 0;
+        for (int col = 0; col < n; ++col) {
+            auto& it = item[row][col];
+            size += sprintf(buf+size, "%d ", values[it]);
+            assert(buf[size] == '\0');
+        }
+        printf("%s\n", buf);
+    }
+    return 0;
+}
