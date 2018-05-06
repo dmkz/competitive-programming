0ddba21e85ca6043f0837a3b6c19d09bf3d9c821
Dmitry <dmkozyrev@rambler.ru>
2018-05-06 19:09:10 +0300

    "Двуграмма": поиск максимума

--- codeforces.com/Codeforces Round #479 (Div. 3)/b.cpp
+++ codeforces.com/Codeforces Round #479 (Div. 3)/b.cpp
@@ -3,10 +3,12 @@
 int main() {
     scanf("%*d");
     std::string s; std::cin >> s;
+    // Посчитаем количество всех подстрок длины 2:
     std::map<std::string, int> count;
     for (int i = 0; i < (int)s.size()-1; ++i) {
         count[s.substr(i, 2)]++;
     }
+    // Найдем максимум:
     int max_count = 0; std::string max_s;
     for (auto& it : count) {
         if (it.second > max_count) {
@@ -14,6 +16,7 @@ int main() {
             max_count = it.second;
         }
     }
+    // Вывод ответа:
     std::cout << max_s << std::endl;
     return 0;
 }
