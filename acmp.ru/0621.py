c38b517958123d459ac8f16186c2148521f1cba2
Dmitry <dmkozyrev@rambler.ru>
2018-05-12 19:30:30 +0300

    "Ближайшее число": двумерное динамическое программирование

--- acmp.ru/0621.py
+++ acmp.ru/0621.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
-from copy import *
 import os, sys
 
 # Reading all input as array:
-temp = [s.split() for s in sys.stdin.read().split('\n')]
+arr = [[int (x) for x in s.split()] for s in sys.stdin.read().split('\n')]
 
 # Copy size of array:
-n = int(temp[0][0])
+n = arr[0][0]
+arr = arr[1:]
 
-# Reading input array[n][n]:
-arr = [[0 for _ in range(n)] for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        arr[i][j] = int(temp[1+i][j])
-    
+# Copy:
+def copy(a):
+    return a.copy()
+        
 # Struct Record:
 class Record:
     
     def __init__(self, dist = 10000, count = 2, row = -1, col = -1):
         self.dist, self.count, self.row, self.col = dist, count, row, col
-        
+    
+    def copy(self):
+        return Record(**self.__dict__)
+    
     def inc(self):
         self.dist = self.dist + 1
         return self
@@ -41,22 +42,28 @@ best_l_d = [[Record() for _ in range(n)] for _ in range(n)]
 best_r_u = [[Record() for _ in range(n)] for _ in range(n)]
 best_r_d = [[Record() for _ in range(n)] for _ in range(n)]
 
-# Walk from left-up corner:
-best_l_u[0][0] = Record(0, 1, 0, 0) if (arr[0][0] != 0) else Record()
+for i in range(n):
+    for j in range(n):
+        if (arr[i][j] != 0):
+            best_l_u[i][j] = Record(0, 1, i, j)
+            best_l_d[i][j] = Record(0, 1, i, j)
+            best_r_u[i][j] = Record(0, 1, i, j)
+            best_r_d[i][j] = Record(0, 1, i, j)
 
+# Walk from left-up corner:
 for i in range(1, n):
     best_l_u[i][0] = Record(0, 1, i, 0) if (arr[i][0] != 0) else copy(best_l_u[i-1][0]).inc()
 
 for j in range(1, n):
-    best_l_u[0][j] = Record(0, 1, 0, j) if(arr[0][j] != 0) else copy(best_l_u[0][j-1]).inc()
+    best_l_u[0][j] = Record(0, 1, 0, j) if (arr[0][j] != 0) else copy(best_l_u[0][j-1]).inc()
     
 for i in range(1, n):
     for j in range(1, n):
-        best_l_u[i][j] = Record(0, 1, i, j) if (arr[i][j] != 0) else combine(best_l_u[i-1][j], best_l_u[i][j-1]).inc()
+        temp = combine(best_l_u[i-1][j], best_l_u[i][j-1]).inc()
+        if (temp.dist < best_l_u[i][j].dist):
+            best_l_u[i][j] = temp
 
 # Walk from left-down corner:
-best_l_d[n-1][0] = Record(0, 1, n-1, 0) if (arr[n-1][0] != 0) else Record()
-
 for i in range(n-2, -1, -1):
     best_l_d[i][0] = Record(0, 1, i, 0) if (arr[i][0] != 0) else copy(best_l_d[i+1][0]).inc()
 
@@ -65,11 +72,11 @@ for j in range(1, n):
 
 for i in range(n-2, -1, -1):
     for j in range(1, n):
-        best_l_d[i][j] = Record(0, 1, i, j) if (arr[i][j] != 0) else combine(best_l_d[i+1][j], best_l_d[i][j-1]).inc()
+        temp = combine(best_l_d[i+1][j], best_l_d[i][j-1]).inc()
+        if (temp.dist < best_l_d[i][j].dist):
+            best_l_d[i][j] = temp
 
 # Walk from right-down corner:
-best_r_d[n-1][n-1] = Record(0, 1, n-1, n-1) if (arr[n-1][n-1] != 0) else Record()
-
 for i in range(n-2, -1, -1):
     best_r_d[i][n-1] = Record(0, 1, i, n-1) if (arr[i][n-1] != 0) else copy(best_r_d[i+1][n-1]).inc()
 
@@ -78,11 +85,11 @@ for j in range(n-2, -1, -1):
     
 for i in range(n-2, -1, -1):
     for j in range(n-2, -1, -1):
-        best_r_d[i][j] = Record(0, 1, i, j) if (arr[i][j] != 0) else combine(best_r_d[i+1][j], best_r_d[i][j+1]).inc()
+        temp = combine(best_r_d[i+1][j], best_r_d[i][j+1]).inc()
+        if (temp.dist < best_r_d[i][j].dist):
+            best_r_d[i][j] = temp
 
 # Walk from right-up corner:
-best_r_u[0][n-1] = Record(0, 1, 0, n-1) if (arr[0][n-1] != 0) else Record()
-
 for i in range(1, n):
     best_r_u[i][n-1] = Record(0, 1, i, n-1) if (arr[i][n-1] != 0) else copy(best_r_u[i-1][n-1]).inc()
     
@@ -91,7 +98,9 @@ for j in range(n-2, -1, -1):
     
 for i in range(1, n):
     for j in range(n-2, -1, -1):
-        best_r_u[i][j] = Record(0, 1, i, j) if (arr[i][j] != 0) else combine(best_r_u[i-1][j], best_r_u[i][j+1]).inc();
+        temp = combine(best_r_u[i-1][j], best_r_u[i][j+1]).inc();
+        if (temp.dist < best_r_u[i][j].dist):
+            best_r_u[i][j] = temp
 
 # Combine answers for each corners:
 for i in range(0, n):
