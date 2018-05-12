d55c5fd925546ab7a636cc30065773f53b8ae3d4
Dmitry <dmkozyrev@rambler.ru>
2018-05-12 17:42:59 +0300

    "Ближайшее число": двумерное динамическое программирование

--- acmp.ru/0621-dp.cpp
+++ acmp.ru/0621-dp.cpp
@@ -3,26 +3,26 @@
 #include <bits/stdc++.h>
 
 struct Record {
-	int dist, count, row, col;
-	
-	Record(int dist = 500, int count = 2, int row = -1, int col = -1)
-		: dist(dist)
-		, count(count)
-		, row(row)
-		, col(col)
-	{ }
+    int dist, count, row, col;
+    
+    Record(int dist = 500, int count = 2, int row = -1, int col = -1)
+        : dist(dist)
+        , count(count)
+        , row(row)
+        , col(col)
+    { }
 };
 
 Record combine(const Record& a, const Record& b) {
-	if (a.dist > b.dist) {
-		return b;
-	} else if (a.dist < b.dist) {
-		return a;
-	} else if (a.row == b.row && a.col == b.col) {
+    if (a.dist > b.dist) {
+        return b;
+    } else if (a.dist < b.dist) {
+        return a;
+    } else if (a.row == b.row && a.col == b.col) {
         return a;
     } else {
-		return Record(a.dist, a.count+b.count, -1, -1);
-	}
+        return Record(a.dist, a.count+b.count, -1, -1);
+    }
 }
 
 std::vector<std::vector<Record>> dp(int sr, int dr, int tr, int sc, int dc, int tc, const std::vector<std::vector<int>>& arr) {
@@ -51,18 +51,18 @@ std::vector<std::vector<Record>> dp(int sr, int dr, int tr, int sc, int dc, int 
 
 std::vector<std::vector<int>> solve_dp(std::vector<std::vector<int>> arr) {
     const int n = (int)arr.size();
-	// Walk from left-up corner:
+    // Walk from left-up corner:
     auto best_l_u = dp(  0,  1,  n,   0,  1,  n, arr);
-	// Walk from left-down corner:
-	auto best_l_d = dp(n-1, -1, -1,   0,  1,  n, arr);
-	// Walk from right-up corner:
-	auto best_r_u = dp(  0,  1,  n, n-1, -1, -1, arr);
-	// Walk from right-down corner:
-	auto best_r_d = dp(n-1, -1, -1, n-1, -1, -1, arr);
+    // Walk from left-down corner:
+    auto best_l_d = dp(n-1, -1, -1,   0,  1,  n, arr);
+    // Walk from right-up corner:
+    auto best_r_u = dp(  0,  1,  n, n-1, -1, -1, arr);
+    // Walk from right-down corner:
+    auto best_r_d = dp(n-1, -1, -1, n-1, -1, -1, arr);
     // Combine all answers:
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (arr[i][j] == 0) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (arr[i][j] == 0) {
                 auto best = combine(
                     combine(best_l_u[i][j], best_r_u[i][j]),
                     combine(best_l_d[i][j], best_r_d[i][j])
@@ -72,27 +72,27 @@ std::vector<std::vector<int>> solve_dp(std::vector<std::vector<int>> arr) {
                     arr[i][j] = arr[best.row][best.col];
                 }
             }
-		}
-	}
+        }
+    }
     return arr;
 }
 
 int main() {
-	int n; 
-	scanf("%d", &n);
-	
-	std::vector<std::vector<int>> arr(n, std::vector<int>(n));
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			scanf("%d", &arr[i][j]);
-		}
-	}
-	auto arr1 = solve_dp(arr);
+    int n; 
+    scanf("%d", &n);
+    
+    std::vector<std::vector<int>> arr(n, std::vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    auto arr1 = solve_dp(arr);
     for (auto& row : arr1) {
         for (auto& it : row) {
             printf("%d ", it);
         }
         printf("\n");
     }
-	return 0;
+    return 0;
 }
