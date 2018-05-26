43f92ed281b7c09641ae0e09fc7e7a29ac531375
Dmitry <dmkozyrev@rambler.ru>
2018-05-26 22:59:35 +0300

    "НОД с изменением элемента": sqrt-декомпозиция

---
+++ acmp.ru/1187.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int rem = a % b;
+        a = b;
+        b = rem;
+    }
+    return a;
+}
+
+struct DataStruct {
+    std::vector<int> arr, gcd;
+    
+    const int GSIZE = 128;
+    
+    DataStruct(int size = 0, int item = 0) {
+        arr.assign(size, item);
+        gcd.assign(size / GSIZE+1, item);
+    }
+    
+    void set(int p, int v) {
+        arr[p] = v;
+        const int g = p / GSIZE;
+        gcd[g] = v;
+        const int begin = g * GSIZE;
+        const int after = std::min(begin + GSIZE, (int)arr.size());
+        for (int i = begin; i < after; ++i) {
+            gcd[g] = ::gcd(arr[i], gcd[g]);
+        }
+    }
+    
+    int get(int l, int r) {
+        const int gl = l / GSIZE;
+        const int gr = r / GSIZE;
+        int answ = arr[l];
+        if (gl == gr) {
+            for (int i = l; i <= r; ++i) {
+                answ = ::gcd(answ, arr[i]);
+            }
+        } else {
+            for (int i = l, after = (gl+1)*GSIZE; i < after; ++i) {
+                answ = ::gcd(answ, arr[i]);
+            }
+            for (int g = gl+1; g < gr; ++g) {
+                answ = ::gcd(answ, gcd[g]);
+            }
+            for (int i = gr * GSIZE; i <= r; ++i) {
+                answ = ::gcd(answ, arr[i]);
+            }
+        }
+        return answ;
+    }
+    
+};
+
+int main() {
+    int n; 
+    scanf("%d", &n);
+    DataStruct ds(n, 0);
+    for (int i = 0; i < n; ++i) {
+        int value;
+        scanf("%d", &value);
+        ds.set(i, value);
+    }
+    int q;
+    scanf("%d", &q);
+    while (q--) {
+        char t; int l, r;
+        scanf(" %c %d %d", &t, &l, &r);
+        if (t == 'u') {
+            ds.set(l-1, r);
+        } else {
+            printf("%d ", ds.get(l-1, r-1));
+        }
+    }
+    return 0;
+}
