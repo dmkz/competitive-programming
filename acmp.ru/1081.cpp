01d75279aae41b8435027b85de557c8376dd001e
Dmitry <dmkozyrev@rambler.ru>
2018-05-26 13:23:18 +0300

    "Суммы на отрезках": префикс-суммы

---
+++ acmp.ru/1081.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <vector>
+typedef long long ll;
+int main() {
+    int n; 
+    scanf("%d", &n);
+    std::vector<ll> sum(1+n);
+    std::vector<int> arr(1+n); 
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &arr[i]);
+        sum[i] = sum[i-1] + arr[i];
+    }
+    int nQueries; 
+    scanf("%d", &nQueries);
+    while (nQueries--) {
+        int l, r; 
+        scanf("%d %d", &l, &r);
+        printf("%I64d\n", sum[r] - sum[l-1]);
+    }
+    return 0;
+}
