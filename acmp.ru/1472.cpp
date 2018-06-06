a4ff4e4253896e506f21a307125183c58d1843e7
Dmitry <dmkozyrev@rambler.ru>
2018-06-07 00:08:08 +0300

    "Системы счисления": представление числа в системе счисления

---
+++ acmp.ru/1472.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int number, need;
+    std::cin >> number >> need;
+    // number % base == need
+    // number == need + base * t
+    // (number - need) = base * t
+    std::vector<int> answer;
+    int a = need % 10, b = need / 10;
+    // number = a + b * base + base^2*(...)
+    for (int base = 2; a + b * base + base * base <= number; ++base) {
+        if ((number - a - b * base) % (base*base) == 0 && a < base && b < base) {
+            answer.push_back(base);
+        }
+    }
+    int base = (number - a) / b;
+    if (number == a + b * base && a < base && b < base) {
+        answer.push_back(base);
+    }
+    if (answer.empty()) {
+        answer.push_back(-1);
+    }
+    for (auto& it : answer) {
+        printf("%d ", it);
+    }
+    return 0;
+}
