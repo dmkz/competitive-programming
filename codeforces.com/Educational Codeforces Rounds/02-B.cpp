7a9d15d717279b4a7c36fa38d6a3382d11f7173f
Dmitry <dmkozyrev@rambler.ru>
2018-07-19 01:26:02 +0300

    sorting, binary search, O(n log(n))

---
+++ codeforces.com/Educational Codeforces Rounds/02-B.cpp
@@ -0,0 +1,32 @@
+/*
+    Problem: 600B. Queries about less or equal elements
+
+    Solution: sorting, binary search, O(n log(n))
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int len_a, len_b;
+    std::cin >> len_a >> len_b;
+    
+    std::vector<int> a(len_a), b(len_b);
+    for (auto & it : a) {
+        std::cin >> it;
+    }
+    
+    for (auto & it : b) {
+        std::cin >> it;
+    }
+    
+    std::sort(a.begin(), a.end());
+    
+    for (auto & it : b) {
+        auto jt = std::upper_bound(a.begin(), a.end(), it);
+        std::cout << int(jt - a.begin()) << " ";
+    }
+    
+    return 0;
+}
