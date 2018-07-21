658261fb5270950b6146cddeb25680ce8a82ef12
Dmitry <dmkozyrev@rambler.ru>
2018-07-21 14:44:59 +0300

    implementation, O(n^2*q)

---
+++ codeforces.com/Educational Codeforces Rounds/12-B.cpp
@@ -0,0 +1,40 @@
+/*
+    Problem: 665B. Shopping
+    
+    Solution: implementation, O(n^2*q)
+    
+    Author: Dmitry Kozyrev, https://github.com/dmkz , dmkozyrev@rambler.ru
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+typedef long long ll;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0); std::cerr.tie(0);
+    
+    int n, nQueries, nItems;
+    std::cin >> n >> nQueries >> nItems;
+    
+    std::vector<int> items(nItems);
+    
+    for (auto& it : items) {
+        std::cin >> it;
+    }
+    
+    int total = 0;
+    while (n--) {
+        for (int i = 0; i < nQueries; ++i) {
+            int value; std::cin >> value;
+            auto it = std::find(items.begin(), items.end(), value);
+            total += int(it - items.begin())+1;
+            std::rotate(items.begin(), it, it+1);
+        }
+    }
+    
+    std::cout << total;
+    return 0;
+}
