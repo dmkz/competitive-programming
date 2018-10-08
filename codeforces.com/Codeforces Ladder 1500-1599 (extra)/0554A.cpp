d6e472e706a03f6513c925c7aaafe55e01ca591e
Dmitry <dmkozyrev@rambler.ru>
2018-10-08 10:17:16 +0300

    implementation, strings, O(n)

---
+++ codeforces.com/Codeforces Ladder 1500-1599 (extra)/0554A.cpp
@@ -0,0 +1,34 @@
+/*
+    Problem: 554A. Kyoya and Photobooks
+    
+    Solution: implementation, strings, O(n)
+    
+    Author: Dmitry Kozyrev, github: dmkz, e-mail: dmkozyrev@rambler.ru
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cassert>
+
+#define ios_fast std::ios_base::sync_with_stdio(0); \
+std::cin.tie(0); std::cout.tie(0); std::cerr.tie(0);
+
+#define  input(x) for (auto& it : (x)) std::cin >> it
+#define    all(x) (x).begin(), (x).end()
+#define   size(x) (int)((x).size())
+#define unique(x) (x).erase(std::unique(all((x))), (x).end())
+
+const int mod = (int)1e9+7;
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<vi> vvi;
+typedef std::vector<ll> vll;
+
+int main() {
+    std::string s; std::cin >> s;
+    std::cout << 26*(size(s)+1)-size(s);
+    
+    return 0;
+}
