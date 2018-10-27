98c932a2ec62dd3d0090430ec1dc88de081b4088
Dmitry <dmkozyrev@rambler.ru>
2018-10-27 20:47:08 +0300

    topological sorting, trees, greedy, dfs, O(n)

---
+++ codeforces.com/Codeforces Ladder 1600-1699/0274B.cpp
@@ -0,0 +1,61 @@
+/*
+    Problem: 274B. Zero Tree
+    
+    Solution: topological sorting, trees, greedy, dfs, O(n)
+    
+    Author: Dmitry Kozyrev, github: dmkz, e-mail: dmkozyrev@rambler.ru
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <functional>
+
+typedef long long ll;
+
+int main() {
+    // Input tree:
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0); std::cerr.tie(0);
+    int n; std::cin >> n;
+    std::vector<std::vector<int>> next(1+n);
+    for (int i = 1, u, v; i < n; ++i) {
+        std::cin >> u >> v;
+        next[u].push_back(v);
+        next[v].push_back(u);
+    }
+    // Topological sorting:
+    std::vector<int> order, parent(1+n, -1);
+    std::function<void(int,int)> topsort = [&](int u, int p) {
+        if (parent[u] != -1) return;
+        parent[u] = p;
+        for (int v : next[u]) {
+            if (v == p) continue;
+            topsort(v, u);
+        }
+        order.push_back(u);
+    };
+    topsort(1, 0);
+    // Greedy over order of topsorting:
+    std::vector<ll> inc(1+n), dec(1+n), value(1+n);
+    ll nInc = 0, nDec = 0;
+    for (int i = 1; i <= n; ++i) {
+        std::cin >> value[i];
+    }
+    for (int u : order) {
+        ll val = value[u] + inc[u] - dec[u];
+        if (val > 0) {
+            dec[u] += val;
+        } else {
+            inc[u] -= val;
+        }
+        nInc = std::max(nInc, inc[u]);
+        nDec = std::max(nDec, dec[u]);
+        int p = parent[u];
+        inc[p] = std::max(inc[p], inc[u]);
+        dec[p] = std::max(dec[p], dec[u]);
+    }
+    std::cout << nInc + nDec << std::endl;
+    return 0;
+}
