cf775b1691f55bb4ac82b7313db29590877cab5c
Dmitry <dmkozyrev@rambler.ru>
2019-01-24 13:19:09 +0300

    разбор выражений, строки, O(|s| * log(|s|))

--- acmp.ru/1407.cpp
+++ acmp.ru/1407.cpp
@@ -1,120 +1,118 @@
+/*
+    Задача: 1407. Производная
+    
+    Решение: разбор выражений, строки, O(|s|*log(|s|))
+    
+    Автор: Дмитрий Козырев, github: dmkz, e-mail: dmkozyrev@rambler.ru
+*/
+
 #include <iostream>
+#include <map>
 #include <string>
 #include <cassert>
-#include <vector>
 
-void assert_tle(bool eq) {
-    if (!eq) while (true);
+typedef long long ll;
+
+namespace Solution {
+
+    int p; std::string data; std::map<ll,ll> coeff;
+
+    bool input() {
+        p = 0; coeff.clear();
+        std::getline(std::cin, data);
+        return !!(std::cin);
+    }
+    
+    bool getTerm() {
+        if (p == (int)data.size()) {
+            return false;
+        }
+        assert(data[p] == '+' || data[p] == '-' || data[p] == 'x' || ('0' <= data[p] && data[p] <= '9'));
+        bool positive = !(data[p] == '-');
+        p += (data[p] == '+' || data[p] == '-');
+        assert(p < (int)data.size());
+        assert(('0' <= data[p] && data[p] <= '9') || (data[p] == 'x'));
+        ll c = ((data[p] == 'x') ? (1) : (0));
+        while ('0' <= data[p] && data[p] <= '9') {
+            (c *= 10) += (data[p] - '0'); ++p;
+        }
+        assert(p == (int)data.size() || data[p] == 'x' || data[p] == '+' || data[p] == '-');
+        if (!positive) { c = -c; }
+        if (p == (int)data.size() || data[p] == '+' || data[p] == '-') {
+            coeff[0] += c;
+            return true;
+        }
+        ++p;
+        assert(p == (int)data.size() || data[p] == '^' || data[p] == '+' || data[p] == '-');
+        if (p == (int)data.size() || data[p] == '+' || data[p] == '-') {
+            coeff[1] += c;
+            return true;
+        }
+        ++p;
+        ll power = 0;
+        assert(p < (int)data.size() && '0' <= data[p] && data[p] <= '9');
+        while (p < (int)data.size() && '0' <= data[p] && data[p] <= '9') {
+            (power *= 10) += (data[p] - '0'); ++p;
+        }
+        assert(p == (int)data.size() || data[p] == '+' || data[p] == '-');
+        assert(power >= 0);
+        coeff[power] += c;
+        return true;
+    }
+    
+    std::string solve() {
+        { // normalize input string
+            std::string alp = "-+^0123456789x";
+            std::string temp = data;
+            data.clear();
+            for (auto &it : temp) { 
+                if (alp.find(it) != std::string::npos) {
+                    if (data.empty()) {
+                        data.push_back(it);
+                        continue;
+                    }
+                    if (it == '+' && data.back() == '+') {
+                        continue;
+                    }
+                    data.push_back(it);
+                }
+            }
+            for (int i = 0; i+1 < (int)data.size(); ++i) {
+                assert(!(data[i] == '+' && data[i+1] == '+'));
+                assert(!((data[i] == '+' || data[i] == '-') && (data[i+1] == '+' || data[i+1] == '-')));
+            }
+        }
+        while (getTerm()) {}
+        for (auto &it : coeff) {
+            auto pair = it;
+            coeff[pair.first] -= pair.second;
+            coeff[pair.first-1] += pair.first * pair.second;
+        }
+        std::string answer;
+        for (auto it = coeff.rbegin(); it != coeff.rend(); it++) {
+            if (it->second == 0 || it->first < 0) { continue; }
+            ll c = it->second;
+            ll p = it->first;
+            answer.push_back(c > 0 ? '+' : '-');
+            if (c < 0) { c = -c; }
+            if (c != 1 || (c == 1 && p == 0)) {
+                answer += std::to_string(c);
+            }
+            if (p != 0) {
+                answer += "x";
+            }
+            if (p <= 1) {
+                continue;
+            }
+            answer += "^" + std::to_string(p);
+        }
+        if (answer.empty()) { answer = "0"; }
+        if (answer[0] == '+') { answer.erase(0, 1); }
+        return answer;
+    }
 }
 
 int main() {
-    // [число][x]^(степень)
-    std::string s;
-    std::getline(std::cin, s);
-    assert_tle(s.size() >= 1u);
-    while (s.back() == 13 || s.back() == 10) {
-        s.pop_back();
-        assert_tle(!s.empty());
-    }
-    std::string temp;
-    for (int i = 0; i < (int)s.size(); ++i) {
-        if (i == (int)s.size()-1) {
-            temp.push_back(s[i]);
-            break;
-        }
-        if (s[i] == '+' && s[i+1] == '+') {
-            temp.push_back('+'); ++i;
-        } else if (s[i] == '+' && s[i+1] == '-') {
-            temp.push_back('-'); ++i;
-        } else if (s[i] == '-' && s[i+1] == '+') {
-            temp.push_back('-'); ++i;
-        } else if (s[i] == '-' && s[i+1] == '-') {
-            temp.push_back('-'); ++i;
-        } else {
-            temp.push_back(s[i]);
-        }
-    }
-    s = temp;
-    for (int i = 0; i+1 < (int)s.size(); ++i) {
-        if ((s[i] == '+' || s[i] == '-') && (s[i+1] == '+' || s[i+1] == '-')) {
-            while (true);
-            assert(s[i] == '+' && s[i+1] == '-');
-        }
-    }
-    if (s[0] != '-') {
-        assert_tle(s[0] != '+');
-        s = '+' + s;
-    }
-    s.push_back('?');
-    assert_tle(s.size() > 2u);
-    std::vector<int> x(1+10000);
-    for (int p = 0; p < (int)s.size(); ) {
-        if (s[p] == '?') {
-            break;
-        }
-        assert_tle(s[p] == '+' || s[p] == '-');        
-        bool positive = (s[p] == '+');
-        int coeff = 0, degree = 0, j = p+1;
-        while ('0' <= s[j] && s[j] <= '9') {
-            (coeff *= 10) += (s[j] - '0');
-            ++j;
-        }
-        if (j == p+1) {
-            assert_tle(!((s[j-1] == '+' || s[j-1] == '-') && (s[j]=='+' || s[j] == '-')));
-            assert(s[j] != '-' && s[j] != '+');
-            assert_tle(s[j] == 'x');
-            ++j;
-            coeff = positive ? 1 : -1;
-            degree = 1;
-        } else if (s[j] == 'x') {
-            degree = 1;
-            ++j;
-            coeff = positive ? coeff : -coeff;
-        } else {
-            degree = 0;
-            coeff = positive ? coeff : -coeff;
-        }
-        p = j;
-        if (s[p] == '^') {
-            degree = 0;
-            j = p+1;
-            while ('0' <= s[j] && s[j] <= '9') {
-                (degree *= 10) += (s[j] - '0');
-                ++j;
-            }
-            p = j;
-        }
-        if (degree > 0) {
-            x[degree-1] += degree * coeff;
-            //std::cout << "to degree " << degree-1 << " add " << degree * coeff << std::endl;
-        }
-    }
-    
-    std::string answ;
-    for (int d = 10000; d >= 0; --d) {
-        if (x[d] == 0) {
-            continue;
-        }
-        std::string temp = (x[d] > 0 ? "+" : "--");
-        x[d] = std::abs(x[d]);
-        if (x[d] != 1 || d == 0) {
-            temp += std::to_string(x[d]);
-        }
-        //std::cout << temp << std::endl;
-        if (d > 0) {
-            temp += 'x';
-            if (d > 1) {
-                temp += '^' + std::to_string(d);
-            }
-        }
-        answ += temp;
-    }
-    if (answ.empty()) {
-        answ = "0";
-    }
-    if (answ[0] == '+') {
-        answ = answ.substr(1);
-    }
-    std::cout << answ;
+    while (Solution::input()) { std::cout << Solution::solve() << std::endl; }
     return 0;
 }
