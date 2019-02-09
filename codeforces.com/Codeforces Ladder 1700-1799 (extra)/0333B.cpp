4708dcf2b2ae3ab05c36c255d0a4a8f8e4ec3378
Dmitry <dmkozyrev@rambler.ru>
2019-02-09 10:29:51 +0300

    greedy, constructive, O(n+m)

--- codeforces.com/Codeforces Ladder 1700-1799 (extra)/0333B.cpp
+++ codeforces.com/Codeforces Ladder 1700-1799 (extra)/0333B.cpp
@@ -1,38 +1,32 @@
 /*
-    Problem: 333B. Polyline
+    Problem: 333B. Chips
     
-    Solution: constructive, geometry, brute force, O(n!)
+    Solution: greedy, constructive, O(n+m)
     
     Author: Dmitry Kozyrev, github: dmkz, e-mail: dmkozyrev@rambler.ru
 */
 
 #include <iostream>
-#include <algorithm>
-
-int solve(int x1, int y1, int x2, int y2, int x3, int y3) {
-    if (x1 == x2 && x1 == x3) {
-        return 1;
-    }
-    if (x1 == x2 && (y3 <= std::min(y1,y2) || std::max(y1,y2) <= y3)) {
-        return 2;
-    }
-    return 3;
-}
-
-int solveMain(int x1, int y1, int x2, int y2, int x3, int y3) {
-    return std::min(solve(x1,y1,x2,y2,x3,y3),solve(y1,x1,y2,x2,y3,x3));
-}
+#include <vector>
 
 int main() {
-    int x1, y1, x2, y2, x3, y3;
-    while (std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3) {
-        int res = solveMain(x1,y1,x2,y2,x3,y3);
-        res = std::min(res, solveMain(x1,y1,x3,y3,x2,y2));
-        res = std::min(res, solveMain(x2,y2,x1,y1,x3,y3));
-        res = std::min(res, solveMain(x2,y2,x3,y3,x1,y1));
-        res = std::min(res, solveMain(x3,y3,x1,y1,x2,y2));
-        res = std::min(res, solveMain(x3,y3,x2,y2,x1,y1));
-        std::cout << res << std::endl;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0); std::cerr.tie(0);
+    int n, nBanned;
+    while (std::cin >> n >> nBanned) {
+        std::vector<bool> bannedCol(1+n), bannedRow(1+n);
+        for (int i = 0, r, c; i < nBanned; ++i) {
+            std::cin >> r >> c;
+            bannedRow[r] = bannedCol[c] = true;
+        }
+        int answ = 0;
+        if (n % 2 == 1) { answ += !bannedCol[(n+1)/2] || !bannedRow[(n+1)/2]; }
+        for (int r1 = 2; r1 <= n / 2; ++r1) {
+            int r2 = n - r1 + 1;
+            int c1 = r2, c2 = r1;
+            answ += !bannedRow[r1] + !bannedCol[c1] + !bannedRow[r2] + !bannedCol[c2];
+        }
+        std::cout << answ << std::endl;
     }
     return 0;
 }
