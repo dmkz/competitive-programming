1ea8bf3516767e35c769be5e3bc185194f8ffb3c
Dmitry <dmkozyrev@rambler.ru>
2019-03-24 17:11:00 +0300

    FastIO modifications

--- algos/Fast-Input-Output/FastIO.hpp
+++ algos/Fast-Input-Output/FastIO.hpp
@@ -3,154 +3,139 @@
 #include <bits/stdc++.h>
 
 namespace FastIO {
-    
     struct Reader {
-     
-        FILE* file;
-       
-        std::vector<char> buffer;
-       
-        int pos;
-       
-        char last;
-       
-        void read() { buffer.resize(fread(&buffer[0], 1, (int)buffer.size(), file)); pos = 0; }
-       
-        Reader(FILE* file_ = stdin, const int size_ = 1 << 16) 
-            : file(file_), buffer(size_, '\0'), pos(0), last('?') { read(); }
-       
-        char getChar() {
-            if (pos == (int)buffer.size()) { read(); }
-            return last = ((pos == (int)buffer.size()) ? EOF : buffer[pos++]);
-        }
-        
-        template<typename T>
-        T getInt() {
-            char c = '?';
-            while (!(c == '-' || ('0' <= c && c <= '9'))) { c = getChar(); }
-            bool positive = true;
-            if (c == '-') { positive = false; c = getChar(); }
-            T answ(0);
-            while ('0' <= c && c <= '9') { (answ *= 10) += (c - '0'); c = getChar(); }
-            return positive ? answ : -answ;
-        }
-       
-        template<typename T>
-        T getReal() {
-            auto high = getInt<int64_t>();
-            char c = '?';
-            while (!(c == '-' || ('0' <= c && c <= '9'))) { c = getChar(); }
-            bool positive = true;
-            if (c == '-') {
-                positive = false;
-                c = getChar();
-            }
-            long long first = 0;
-            while ('0' <= c && c <= '9') { (first *= 10) += (c - '0'); c = getChar(); }
-            if (c != '.') {
-                return double(positive ? first : -first);
-            }
-            c = getChar();
-            long long second = 0;
-            long long pow = 1;
-            while ('0' <= c && c <= '9') { (second *= 10) += (c - '0'); c = getChar(); pow *= 10; }
-            double answ = first + (double)second / (double)pow;
-            return positive ? answ : -answ;
-        }
+        private:
+            FILE* file; std::vector<char> buffer; int pos;
+            void read();
+            bool was;
+        public:
+            Reader(FILE* file_ = stdin, const int size_ = 1 << 16)
+                : file(file_), buffer(size_, '\0'), pos(0), was(true) { read(); }
+            operator bool() const { return was; }
+            char getChar();
+            std::string getStr();
+            std::string getLine();
+            template<typename T> T getInt();
+            template<typename T> T getReal();
     };
- 
-struct Output {
- 
-    FILE* file;
-   
-    std::vector<char> buffer;
-   
-    int pos;
-   
-    Output(FILE* file_, const int size_ = 1024*1024)
-        : file(file_)
-        , buffer(size_, '\0')
-        , pos(0)
-    { }
-   
-    inline void flush() {
-        put_char(-1);
-    }
-   
-    inline void put_char(char c) {
-        if (pos == (int)buffer.size() || c == -1) {
-            fwrite(&buffer[0], 1, pos, file);
-            pos = 0;
-        }
-        if (c != -1) {
-    buffer[pos++] = c;
-        }
-    }
-   
-    inline void put_int(int value, int len = 0) {
-        if (value < 0) {
-            put_char('-');
-            value = -value;
-        }
-        char buf[12]; int p = 0;
-        do {
-            buf[p++] = char(value % 10 + '0');
-            value /= 10;
-        } while (value > 0);
-        while (p < len) buf[p++] = '0';
-        while (p > 0) put_char(buf[--p]);
-    }
-   
-    inline void put_ll(long long value, int len = 0) {
-        if (value < 0) {
-            put_char('-');
-            value = -value;
-        }
-        char buf[12]; int p = 0;
-        do {
-            buf[p++] = char(value % 10 + '0');
-            value /= 10;
-        } while (value > 0);
-        while (p < len) buf[p++] = '0';
-        while (p > 0) put_char(buf[--p]);
-    }
-   
-    inline void put_dbl(double value, int precision = 6) {
-        if (value < 0) {
-            put_char('-');
-            value = -value;
-        }
+    struct Writer {
+        private:
+            FILE* file; std::vector<char> buffer; int pos;
+        public:
+            Writer(FILE* file_ = stdout, const int size_ = 1 << 16) 
+                : file(file_), buffer(size_, '\0'), pos(0) { }
+            ~Writer() { flush(); }
+            void flush() { putChar(EOF); }
+            void putChar(char c);
+            void putStr(const std::string&);
+            template<typename T> void putInt(T value, int width = 0, char fill = ' ');
+            template<typename T> void putReal(T value, int precision = 6, int width = 0, char fill = ' ');
+    };
+}
+
+namespace FastIO {
        
-        put_ll((long long)value);
-        value -= (long long)value;
+    void Reader::read() {
+        if (!buffer.empty()) {
+            buffer.resize(fread(&buffer[0], 1, (int)buffer.size(), file)); pos = 0; 
+        }
+    }
        
-        if (precision == 0) {
-            return;
-        }
-       
-        put_char('.');
-        while (precision >= 6) {
-            value *= 1000000;
-            put_int((int)value, 6);
-            value -= (int)value;
-            precision -= 6;
-        }
-        while (precision >= 3) {
-            value *= 1000;
-            put_int((int)value, 3);
-            value -= (int)value;
-            precision -= 3;
-        }
-        while (precision >= 1) {
-            value *= 10;
-            put_char(char((int)value+'0'));
-            value -= (int)value;
-            precision--;
-        }
+    char Reader::getChar() {
+        if (pos == (int)buffer.size()) { read(); }
+        if (pos == (int)buffer.size()) { was = false; return EOF; } 
+        else { was = true; return buffer[pos++]; }
+    }
+        
+    std::string Reader::getStr() {
+        char c = ' ';
+        while (std::isspace(c)) { c = getChar(); }
+        std::string answ;
+        while (c != EOF && !std::isspace(c)) { answ.push_back(c); c = getChar(); }
+        was = !answ.empty();
+        return answ;
+    }
+    
+    std::string Reader::getLine() {
+        char c = '\n';
+        while (c == '\n') { c = getChar(); }
+        std::string answ;
+        while (c != '\n' && c != EOF) { answ.push_back(c); c = getChar(); }
+        was = !answ.empty();
+        return answ;
+    }
+        
+    template<typename T>
+    T Reader::getInt() {
+        char c = '?';
+        while (!(c == '-' || ('0' <= c && c <= '9') || c == EOF)) { c = getChar(); }
+        bool positive = true;
+        if (c == '-') { positive = false; c = getChar(); }
+        T answ(0);
+        bool flag = false;
+        while ('0' <= c && c <= '9') { flag = true; (answ *= 10) += (c - '0'); c = getChar(); }
+        was = flag;
+        return positive ? answ : -answ;
     }
    
-    ~Output() {
-        flush();
+    template<typename T>
+    T Reader::getReal() {
+        bool flag = false;
+        char c = '?';
+        while (!(c == '-' || ('0' <= c && c <= '9') || c == EOF)) { c = getChar(); }
+        bool positive = (c != '-');
+        if (c == '-') { c = getChar(); }
+        long long first = 0;
+        while ('0' <= c && c <= '9') { flag = true; (first *= 10) += (c - '0'); c = getChar(); }
+        was = flag;
+        if (c != '.') { return T(positive ? first : -first); }
+        c = getChar();
+        long long second = 0, pow = 1;
+        while ('0' <= c && c <= '9') { (second *= 10) += (c - '0'); c = getChar(); pow *= 10; }
+        T answ = first + (T)second / (T)pow;
+        return positive ? answ : -answ;
     }
-};
+    
+    void Writer::putChar(char c) {
+        if (pos == (int)buffer.size() || c == EOF) { 
+            fwrite(&buffer[0], 1, pos, file); pos = 0; 
+        }
+        if (c != EOF) { buffer[pos++] = c; }
+    }
+    
+    void Writer::putStr(const std::string& s) {
+        for (auto it : s) { 
+            putChar(it); 
+        } 
+    }
+    
+    template<typename T>
+    void Writer::putInt(T value, int width, char fill) {
+        bool positive = !(value < 0);
+        if (value < 0) { value = -value; }
+        char buf[24]; int p = 0;
+        do { buf[p++] = char(value % 10 + '0'); value /= 10; } while (value > 0);
+        if (!positive) { buf[p++] = '-'; }
+        while (p < width) buf[p++] = fill;
+        while (p > 0) putChar(buf[--p]);
+    }
+    
+    template<typename T>
+    void Writer::putReal(T value, int precision, int width, char fill) {
+        putInt((long long)value, width - precision - 1, fill); 
+        value = std::abs(value-(long long)value);
+        if (precision == 0) { return; }
+        putChar('.');
+        #define PRINT_PART(cnt, pow) \
+            while (precision >= cnt) { \
+                value *= pow; \
+                putInt((int)value, cnt, '0'); \
+                value -= (int)value; \
+                precision -= cnt; \
+            }
+        PRINT_PART(6,1000000)
+        PRINT_PART(3,1000)
+        PRINT_PART(1,10)
+        #undef PRINT_PART
+    }
 }
