1dd43882964cc4292632ab688904a3f643b637c7
Dmitry <dmkozyrev@rambler.ru>
2019-03-29 13:13:44 +0300

    динамическое программирование, жадные алгоритмы, O(n*log(n))

--- acmp.ru/0758.cpp
+++ acmp.ru/0758.cpp
@@ -1,44 +1,33 @@
 /*
-    "Веревочный мост": сортировка, рекурсия, жадный алгоритм, O(n log(n))
+    Задача: 758. Веревочный мост
+    
+    Решение: динамическое программирование, жадные алгоритмы, O(n*log(n))
+    
+    Автор: Дмитрий Козырев, github: dmkz, e-mail: dmkozyrev@rambler.ru
 */
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <functional>
-
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
 typedef long long ll;
-
+typedef std::vector<int> vi;
+typedef std::vector<ll> vl;
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0); std::cout.tie(0); std::cerr.tie(0);
-    
-    int n; std::cin >> n;
-    
-    std::vector<int> time(n);
-    
-    for (auto& it : time) {
-        std::cin >> it;
-    }
-    
-    std::sort(time.begin(), time.end());
-    
-    std::vector<ll> answ(n);
-    
-    std::function<ll(int)> solve = [&](const int size) {
-        if (size < 2) {
-            return (ll)time[size];
+    std::ios_base::sync_with_stdio(false); std::cin.tie(0);
+    for (int n; std::cin >> n; ) {
+        vi a(n); for (auto &it : a) { std::cin >> it; }
+        std::sort(all(a));
+        vl answ(n);
+        answ[0] = a[0];
+        if (n >= 2) {
+            answ[1] = a[1];
         }
-        if (!answ[size]) {
-            answ[size] = std::min(
-                time[1] + time[0] + time[size] + time[1] + solve(size-2), 
-                time[size] + time[0] + solve(size-1)
+        for (int i = 2; i < n; ++i) {
+            answ[i] = std::min(
+                answ[i-1] + a[0] + a[i],
+                answ[i-2] + a[1] + a[0] + a[i] + a[1]
             );
         }
-        return answ[size];
-    };
-    
-    std::cout << solve(n-1);
-    
+        std::cout << answ.back() << std::endl;
+    }
     return 0;
 }
