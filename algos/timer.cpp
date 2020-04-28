fc363b833cb96bb25c484a2cebf42415dbba9316
Dmitry Kozyrev <dmkozyrev@rambler.ru>
2020-04-28 12:50:19 +0300

    Added class Timer

---
+++ algos/timer.cpp
@@ -0,0 +1,12 @@
+class Timer {
+    std::chrono::time_point<std::chrono::steady_clock> timePoint;
+    size_t value;
+public:
+    void start() { timePoint = std::chrono::steady_clock::now(); }
+    void finish() {
+        auto curr = std::chrono::steady_clock::now();    
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(curr - timePoint);
+        value = elapsed.count();
+    }
+    size_t operator()() const { return value; }
+};
