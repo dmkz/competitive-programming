4b44f2fda9c2aef9f658af0ad39d58238300ba65
Dmitry <dmkozyrev@rambler.ru>
2021-09-27 10:39:07 +0100

    математика, O(1)

---
+++ mirea/contests/2021-09-26 MIREA Welcome Contest 2021/dmkozyrev C++/Задача A.cpp
@@ -0,0 +1,8 @@
+// решение: математика, O(1)
+// ссылка: https://codeforces.com/group/LB1sSRhotq/contest/346286/submission/129986172
+#include <bits/stdc++.h>
+int main() {
+    // ответ всегда n-1
+    int n; std::cin >> n;
+    std::cout << n-1 << std::endl;
+}
