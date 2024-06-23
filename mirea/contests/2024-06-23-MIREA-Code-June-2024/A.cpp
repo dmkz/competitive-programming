2d9206e51a4d244e78f9739eb6ec9463fe3ccacf
Dmitrii Kozyrev <dmkozyrev@rambler.ru>
2024-06-23 14:41:50 +0100

    Цитаты Джейсона Стетхэма

---
+++ mirea/contests/2024-06-23-MIREA-Code-June-2024/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+// достаточно было скопировать выходные данные из этой задачи и отправить их
+// но факт, что в вердиктах чекера можно прятать пасхалки, забавный, и будет
+// использован в следующих чемпионатах МИРЭА
+int main() {
+    cout << "Delai, kak nado. Kak ne nado, ne delai.\n";
+    cout << "Esli tebe gdeto ne rady v rvanyh noskah, to i v celyh tuda idti ne stoit.\n";
+    cout << "Esli zabludilsya v lesu - idi domoi.\n";
+    cout << "Mi dolzhni ostavatsya mimi, a oni - onimi.\n";
+    cout << "Rabota - eto ne volk. Rabota - eto work. A volk - eto hodit'. Zapomnite.\n";
+    cout << "Sila ne v babkah, ved' babki uzhe starie.\n";
+    cout << "V zhizni vsegda est' dve dorogi: odna - pervaya, a drugaya - vtoraya.\n";
+    cout << "Vzyal nozh - rezh. Vzyal doshik - esh.\n";
+    cout << "Ya zhivu kak karta lyazhet. Ty zhivesh - kak mamka skazhet.\n";
+    cout << "Zapomni: vsego odna oshibka - i ty oshibsya.\n";
+}
