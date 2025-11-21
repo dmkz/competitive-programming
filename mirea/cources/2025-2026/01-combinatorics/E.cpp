e13ef7ff3fe5d7cd56debca0f4abc693c3514da2
Dmitrii Kozyrev <dmkozyrev@rambler.ru>
2025-11-21 20:57:41 +0300

    Деление

---
+++ mirea/cources/2025-2026/01-combinatorics/E.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MOD = 1000000007LL;
+
+ll mul(ll x, ll y) {
+    return (x * y) % MOD;
+}
+
+ll binpow(ll a, ll n) {
+    a %= MOD;
+    ll res = 1 % MOD;
+    while (n > 0) {
+        if (n & 1)
+            res = mul(res, a);
+        a = mul(a, a);
+        n >>= 1;
+    }
+    return res;
+}
+
+main() {
+    ll a, b;
+    cin >> a >> b;
+    a %= MOD;
+    b %= MOD;
+    ll inv = binpow(b, MOD - 2);
+    ll d = mul(a, inv);
+    cout << d;
+}
