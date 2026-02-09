6202d08213770833eb46b0d25b8ab0b1880b68e8
Dmitrii Kozyrev <dmkozyrev@rambler.ru>
2026-02-09 13:52:09 +0300

    двумерное динамическое программирование, O(N^2)

---
+++ acmp.ru/0113-dp.cpp
@@ -0,0 +1,34 @@
+// двумерное динамическое программирование, O(N^2)
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n; cin >> n;
+    vector<string> a(n);
+    for (auto &s : a)
+        cin >> s;
+    // от каждой клетки считаем, сколько единиц вверх и влево, а также
+    // сторону максимального квадрата с правым нижним углом в этой клетке
+    vector dp(n, vector(n, 0));
+    int answ = 0;
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+            if (a[row][col] == '1') {
+                // кол-во единиц вверх:
+                int U = 1 + (row-1 >= 0 ? dp[row-1][col] : 0);
+                // кол-во единиц влево:
+                int L = 1 + (col-1 >= 0 ? dp[row][col-1] : 0);
+                // максимальная сторона квадрата выбирается как минимум из:
+                // квадрата на одну клетку выше и левее, +1
+                // максимальной полосы единиц вверх
+                // максимальной полосы единиц влево
+                int S = 1 + (row-1>=0&&col-1>=0?dp[row-1][col-1]:0);
+                int res = (dp[row][col] = min({U,L,S}));
+                if (answ < res * res)
+                    answ = res * res;
+            }
+        }
+    }
+    cout << answ << endl;
+}
